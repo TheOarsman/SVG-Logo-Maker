6e1d24f003c0953b41fd3b70d0b4f50f7ba954b6
TheOarsman <heinzulrichv@gmail.com>
2024-03-10 23:34:17 -0400

    handle users answers

--- index.js
+++ index.js
@@ -4,109 +4,139 @@ const { Shape, Circle, Square, Triangle } = require("./lib/shapes.js");
 
 const shapeChoices = { Circle, Square, Triangle };
 
-inquire.createPromptModule([
-  // Choose shape
-  {
-    type: "list",
-    message: "What shape would you like?",
-    name: "shape",
-    choices: shapeChoices,
-  },
+inquire
+    .prompt([
+        // Choose shape
+        {
+            type: "list",
+            message: "What shape would you like?",
+            name: "chooseShape",
+            choices: shapeChoices,
+        },
 
-  // Choose shape color
-  {
-    type: "list",
-    message: "What color would you like the shape to be? Choose color format:",
-    name: "shapeColorChoice",
-    choices: ["color keyword", "hexadecimal"],
-  },
+        // Choose shape color
+        {
+            type: "list",
+            message:
+                "What color would you like the shape to be? Choose color format:",
+            name: "shapeColorChoice",
+            choices: ["color keyword", "hexadecimal"],
+        },
 
-  // Validate shape color
-  {
-    type: "input",
-    name: "shapeColor",
-    message: "Enter the color keyword",
-    when: (answers) => {
-      if (answers.shapeColorChoice === "color keyword") {
-        return true;
-      }
-      return false;
-    },
+        // Validate shape color
+        {
+            type: "input",
+            name: "shapeColor",
+            message: "Enter the color keyword",
+            when: (answers) => {
+                if (answers.shapeColorChoice === "color keyword") {
+                    return true;
+                }
+                return false;
+            },
 
-    validate: (answer) => {
-      let answerLowerCase = answer.toLowerCase();
-      for (var i = 0, len = colorKeyWords.length; i < len; ++i) {
-        if (answerLowerCase.indexOf(colorKeyWords[i]) !== -1) {
-          return true;
+            validate: (answer) => {
+                let answerLowerCase = answer.toLowerCase();
+                for (var i = 0, len = colorKeyWords.length; i < len; ++i) {
+                    if (answerLowerCase.indexOf(colorKeyWords[i]) !== -1) {
+                        return true;
+                    }
+                }
+                return console.log("Please enter a valid color keyword");
+            },
+        },
+        {
+            // hexadecimal
+            type: "input",
+            name: "shapeColor",
+            message: "Enter a hexadecimal number (#CCCCCC)",
+            when: (answers) => {
+                if (answers.shapeColorChoice === "hexadecimal") {
+                    return true;
+                }
+            },
+        },
+
+        // Choose text color
+        {
+            type: "textColorChoice",
+            message: "What color would you like the text to be? Choose color format:",
+            type: "list",
+            choices: ["color keyword", "hexadecimal"],
+        },
+
+        // Validate Text color
+        {
+            type: "input",
+            name: "textColor",
+            message: "Enter the color keyword",
+            when: (answers) => {
+                if (answers.textColorChoice === "color keyword") {
+                    return true;
+                }
+                return false;
+            },
+
+            validate: (answer) => {
+                let answerLowerCase = answer.toLowerCase();
+                for (var i = 0, len = colorKeyWords.length; i < len; ++i) {
+                    if (answerLowerCase.indexOf(colorKeyWords[i]) !== -1) {
+                        return true;
+                    }
+                }
+                return console.log("Please enter a valid color keyword");
+            },
+        },
+        {
+            // hexadecimal
+            type: "input",
+            name: "textColor",
+            message: "Enter a hexadecimal number (#CCCCCC)",
+            when: (answers) => {
+                if (answers.textColorChoice === "hexadecimal") {
+                    return true;
+                }
+            },
+        },
+        {
+            type: "input",
+            message: "Enter text for logo (no more than 3 characters)",
+            name: "text",
+        },
+    ])
+
+    .then((answers) => {
+        switch (answers.shape) {
+            case "Circle":
+                shape = new Circle(
+                    answers.chooseShape,
+                    answers.shapeColorChoice,
+                    answers.text);
+                break;
+            case "Sqaure":
+                shape = new Square(
+                    answers.chooseShape,
+                    answers.shapeColorChoice,
+                    answers.text);
+                break;
+            case "Triangle":
+                shape = new Triangle(
+                    answers.chooseShape,
+                    answers.shapeColorChoice,
+                    answers.text);
+                break;
+            default;
+                console.log("No shape currently selected!")
+                shape = new Shape(answers.chooseShape, answers.shapeColorChoice, answers.text);
+                break;
         }
-      }
-      return console.log("Please enter a valid color keyword");
-    },
-  },
-  {
-    // hexadecimal
-    type: "input",
-    name: "shapeColor",
-    message: "Enter a hexadecimal number (#CCCCCC)",
-    when: (answers) => {
-      if (answers.shapeColorChoice === "hexadecimal") {
-        return true;
-      }
-    },
-  },
+ 
 
-  // Choose text color
-  {
-    type: "textColorChoice",
-    message: "What color would you like the text to be? Choose color format:",
-    type: "list",
-    choices: ["color keyword", "hexadecimal"],
-  },
+        const svgContent = shape.render();
 
-  // Validate Text color
-  {
-    type: "input",
-    name: "textColor",
-    message: "Enter the color keyword",
-    when: (answers) => {
-      if (answers.textColorChoice === "color keyword") {
-        return true;
-      }
-      return false;
-    },
-
-    validate: (answer) => {
-      let answerLowerCase = answer.toLowerCase();
-      for (var i = 0, len = colorKeyWords.length; i < len; ++i) {
-        if (answerLowerCase.indexOf(colorKeyWords[i]) !== -1) {
-          return true;
-        }
-      }
-      return console.log("Please enter a valid color keyword");
-    },
-  },
-  {
-    // hexadecimal
-    type: "input",
-    name: "textColor",
-    message: "Enter a hexadecimal number (#CCCCCC)",
-    when: (answers) => {
-      if (answers.textColorChoice === "hexadecimal") {
-        return true;
-      }
-    },
-  },
-
-  {
-    type: "input",
-    message: "Enter text for logo",
-  },
-]);
-
-const svgContent = shape.render();
-
-fs.writeFile("Logo.svg", svgContent, (err) =>
-  err
-    ? console.error(err)
-    : console.log("SVG Logo & file created successfully!")
-);
+        fs.writeFile("Logo.svg", svgContent, (err) =>
+            err
+                ? console.error(err)
+                : console.log("SVG Logo & file created successfully!")
+        );
+    });
